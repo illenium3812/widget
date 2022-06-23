9a4fa9141b60384f6c51d2bc9312289b7bca101a
Mansoor Khan <41062288+illenium3812@users.noreply.github.com>
2022-06-23 20:04:35 +0500

    Update uberall.py

--- uberall.py
+++ uberall.py
@@ -40,7 +40,7 @@ class Uberall:
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
-        driver = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), options=chrome_options)
         try:
             # parsing the query into variables
             lst = query.split('-')
