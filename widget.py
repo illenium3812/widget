432103f1f2ce871ab19137f7ae7bd301f6fe7fdd
Mansoor Khan <41062288+illenium3812@users.noreply.github.com>
2022-06-23 19:43:19 +0500

    Update widget.py

--- widget.py
+++ widget.py
@@ -6,14 +6,11 @@ app = FastAPI()
 uberall = Uberall()
 
 
+@app.get("/")
+async def home():
+    return "Waiting for the query"
+
+
 @app.get("/{query}")
 async def get_results(query):
     return uberall.make_call(query)
-
-
-@app.get("/test/")
-async def test():
-    return {
-        'id': 'hello',
-        'name': 'test'
-    }
