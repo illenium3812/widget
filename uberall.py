7b81f948d232e6bf9d18a57183228aea7f5c4aa4
Mansoor Khan <41062288+illenium3812@users.noreply.github.com>
2022-06-23 20:10:16 +0500

    Update uberall.py

--- uberall.py
+++ uberall.py
@@ -7,6 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 import time
+import os
 
 class Uberall:
 
