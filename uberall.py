c7978aa198f9243aff1e9ca9a99525001be8b71a
Mansoor Khan <41062288+illenium3812@users.noreply.github.com>
2022-06-30 03:44:17 +0500

    Update uberall.py

--- uberall.py
+++ uberall.py
@@ -12,16 +12,7 @@ import os
 
 class Uberall:
     # Setting up Dowloading Directory and Chrome Options
-    def __init__(self):
-        chrome_options = webdriver.ChromeOptions()
-        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-        chrome_options.add_argument('--headless')
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument('--disable-gpu')
-        chrome_options.add_argument("--disable-dev-shm-usage")
-        self.browser = webdriver.Chrome(executable_path=os.environ.get(
-            "CHROMEDRIVER_PATH"), options=chrome_options)
-        self.browser.get("https://uberall.com/en/developers/statusCheckWidget")
+ 
 
     def get_results(self, results):
         search_results = {}
@@ -47,6 +38,16 @@ class Uberall:
         return search_results
 
     def make_call(self, query):
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+        chrome_options.add_argument('--headless')
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        browser = webdriver.Chrome(executable_path=os.environ.get(
+            "CHROMEDRIVER_PATH"), options=chrome_options)
+        browser.get("https://uberall.com/en/developers/statusCheckWidget")
+        
         try:
             # parsing the query into variables
             lst = query.split('-')
@@ -59,35 +60,35 @@ class Uberall:
 
         # Opening up the website
 
-        WebDriverWait(self.browser, 30).until(
+        WebDriverWait(browser, 30).until(
             EC.presence_of_element_located((By.CLASS_NAME, 'Select-control')))
-        self.browser.find_element_by_xpath(
+        browser.find_element_by_xpath(
             "//div[@class='Select-control']").find_element_by_css_selector('input').send_keys(Country)
-        self.browser.find_element_by_class_name('Select-menu-outer').click()
-        self.browser.find_element_by_xpath(
+        browser.find_element_by_class_name('Select-menu-outer').click()
+        browser.find_element_by_xpath(
             '//*[@id="widget-preview-box"]/div/div/div[1]/span/div/div/form/div[2]/input').send_keys(Company_name)
-        self.browser.find_element_by_xpath(
+        browser.find_element_by_xpath(
             '//*[@id="widget-preview-box"]/div/div/div[1]/span/div/div/form/div[3]/input').send_keys(Street_Number)
-        self.browser.find_element_by_xpath(
+        browser.find_element_by_xpath(
             '//*[@id="widget-preview-box"]/div/div/div[1]/span/div/div/form/div[4]/input').send_keys(ZIP)
-        self.browser.find_element_by_xpath(
+        browser.find_element_by_xpath(
             '//input[@value="CHECK NOW"]').click()
         try:
             time.sleep(3)
-            map_inputs = self.browser.find_elements_by_class_name(
+            map_inputs = browser.find_elements_by_class_name(
                 'normalization-map-input')
             map_inputs[0].clear()
             map_inputs[0].send_keys(Street_Number)
             map_inputs[1].clear()
             map_inputs[1].send_keys(Street_Number)
-            self.browser.find_element_by_class_name(
+            browser.find_element_by_class_name(
                 'normalization-map-button').click()
         except Exception as e:
             pass
-        WebDriverWait(self.browser, 60).until(EC.presence_of_element_located(
+        WebDriverWait(browser, 60).until(EC.presence_of_element_located(
             (By.CLASS_NAME, 'ubsc_results-table-wrapper')))
         time.sleep(3)
-        results = self.browser.find_elements_by_class_name(
+        results = browser.find_elements_by_class_name(
             "ubsc_result-listing-row")
         search_results = self.get_results(results)
         return search_results
